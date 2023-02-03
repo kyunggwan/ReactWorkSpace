e974af48ed58a53b19d59ca4f0d1e3f286e58733
kyunggwan <4562sky@naver.com>
2023-02-03 20:34:10 +0900

    usestate

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './App.css';
 import Box from "./Box";
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increase = () => {
+    setCount(count + 1)
+    console.log(count)
+  }
+
   return (
 
-//app에서 다이나믹한 값을 줄때, props를 쓴다
-    <main>
-      <Box name="코알라" num="1"/>
-      <Box name="지민" num="2"/>
-      <Box name="정국" num="3"/>
-      <Box name="뷔" num="4"/>
-    </main>
+//1. app에서 다이나믹한 값을 줄때, props를 쓴다
+    // <main>
+    //   <Box name="코알라" num="1"/>
+    //   <Box name="지민" num="2"/>
+    //   <Box name="정국" num="3"/>
+    //   <Box name="뷔" num="4"/>
+    // </main>
+
+//2. state함수는 비동기적이다.
+//state 함수는 count가 하나 적다.
+      <>
+        <div>{count}</div>
+        <button onClick={increase}>증가</button>
+      </>
   );
 }
 
