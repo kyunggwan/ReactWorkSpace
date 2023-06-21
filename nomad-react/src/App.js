60aa5e0dbec15c449f3da4d217f9b9f82209d94d
kyunggwan <4562sky@naver.com>
2023-06-22 00:10:47 +0900

    feat: useEffect

--- nomad-react/src/App.js
+++ nomad-react/src/App.js
@@ -1,11 +1,39 @@
-import Button from "./Button"
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+  console.log("i run all the time");
+
+  // component 생성되는 처음 한번
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  // keyword가 변할때만 작동
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for ", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"}/>
+      <input
+        type="text"
+        placeholder="search here..."
+        onChange={onChange}
+        value={keyword}
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click!!!</button>
     </div>
   );
 }
