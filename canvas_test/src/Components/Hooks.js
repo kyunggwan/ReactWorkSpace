bf26a61be247dd5a67c6380cf28beacac5f45c18
kyunggwan <4562sky@naver.com>
2023-06-27 16:35:44 +0900

    feat: Canvas의 사각형으로 영점 다시 조절

--- canvas_test/src/Components/Hooks.js
+++ canvas_test/src/Components/Hooks.js
@@ -1,21 +1,32 @@
-import { useRef} from "react";
+import { useRef } from "react";
 
 export function useOnDraw() {
-
   const canvasRef = useRef(null);
 
-  function setCanvasRef(ref){
-    if(!ref) return;
+  function setCanvasRef(ref) {
+    if (!ref) return;
     canvasRef.current = ref;
     initMouseMoveListner();
   }
 
-  function initMouseMoveListner(){
+  function initMouseMoveListner() {
     const mouseMoveListner = (e) => {
-      console.log({x : e.clientX, y: e.clientY});
-    }
+      const point = computePointInCanvas(e.clientX, e.clientY);
+      console.log(point);
+    };
     window.addEventListener("mousemove", mouseMoveListner);
   }
-  
+
+  function computePointInCanvas(clientX, clientY) {
+    if (canvasRef.current) {
+      const boundingRect = canvasRef.current.getBoundingClientRect();
+      return {
+        x: clientX - boundingRect.left, // 사각형으로 좌표 다시 생성
+        y: clientY - boundingRect.top,
+      };
+    } else {
+      return null;
+    }
+  }
   return setCanvasRef;
 }
