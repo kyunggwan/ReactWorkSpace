3ef4d1eceb31f004b9f2859fc26e6d239c34acde
kyunggwan <4562sky@naver.com>
2023-06-27 17:53:07 +0900

    feat: 3번째 연습 시작

--- canvas_test/src/App.js
+++ canvas_test/src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
-import "./App.css";
-import Canvas from "./Components/Canvas";
+import React from 'react'
+import './App.css';
+
 export default function App() {
   return (
-    <div className="App">
-      <Canvas width={700} height={500} />
-    </div>
-  );
+    <div>App</div>
+  )
 }
---
+++ canvas_test/src/Practice2/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import Canvas from "./Components/Canvas";
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas width={700} height={500} />
+    </div>
+  );
+}
