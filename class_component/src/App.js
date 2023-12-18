9c8fcdca3714e27b94c528aef33c470448f813b5
kyunggwan <4562sky@naver.com>
2023-12-18 14:21:53 +0900

    함수에서 state 사용법 hook

--- class_component/src/App.js
+++ class_component/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
@@ -8,14 +8,34 @@ function App() {
       <FuncComp initNumber={3}></FuncComp>
       <ClassComp initNumber={3}></ClassComp>
     </div>
-  )
+  );
 }
 
+/*
+    Functional Component에서는  Hook을 통해서 state 변경 가능
+*/
 function FuncComp(props) {
+  var [number, setNumber] = useState(props.initNumber);
+  var [date, setDate] = useState(new Date().toString());
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {date}</p>
+      <input
+        type="button"
+        value="number"
+        onClick={function () {
+          setNumber(Math.random());
+        }}
+      />
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
+        }}
+      />
     </div>
   );
 }
@@ -24,19 +44,32 @@ function FuncComp(props) {
     Functional Component에서는 아래 처럼 state의 값을 변경할 수 없었음
     단순히 props를 받아서 표시    
 */
-class ClassComp extends React.Component{
+class ClassComp extends React.Component {
   state = {
-    number: this.props.initNumber
-  }
-  render(){
+    number: this.props.initNumber,
+    date: new Date().toString(),
+  };
+  render() {
     return (
-    <div className="container">
-      <h2>class style component</h2>
-      <p>Number : {this.state.number}</p>
-      <input type="button" value="random" onClick={function(){
-          this.setState({number:Math.random()})
-      }.bind(this)}/>
-    </div>
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input
+          type="button"
+          value="random"
+          onClick={function () {
+            this.setState({ number: Math.random() });
+          }.bind(this)}
+        />
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: new Date().toString() });
+          }.bind(this)}
+        />
+      </div>
     );
   }
 }
