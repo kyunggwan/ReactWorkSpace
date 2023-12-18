5bfdec5d0c4311ad13b1a6e0ae3e06e5a83a7074
kyunggwan <4562sky@naver.com>
2023-12-18 14:07:27 +0900

    클래스에서 state 사용법

--- class_component/src/App.js
+++ class_component/src/App.js
@@ -20,12 +20,22 @@ function FuncComp(props) {
   );
 }
 
+/*
+    Functional Component에서는 아래 처럼 state의 값을 변경할 수 없었음
+    단순히 props를 받아서 표시    
+*/
 class ClassComp extends React.Component{
+  state = {
+    number: this.props.initNumber
+  }
   render(){
     return (
     <div className="container">
       <h2>class style component</h2>
-      <p>Number : {this.props.initNumber}</p>
+      <p>Number : {this.state.number}</p>
+      <input type="button" value="random" onClick={function(){
+          this.setState({number:Math.random()})
+      }.bind(this)}/>
     </div>
     );
   }
